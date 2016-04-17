65a40dd027ac54d2c529b4841f3ff7b682ec19db
Xiaofei Cao <xc9pd@mst.edu>
2016-04-18 01:34:52 +0800

    want to create index tree
    I added some comment. Don't know if this is reasonable.

--- src/Index.java
+++ src/Index.java
@@ -5,20 +5,30 @@ import java.util.ArrayList;
  * Created by xc9pd on 4/13/2016.
  */
 public class Index {
-
+    //number of children every nodes usually have
     public long children;
+    //if the number of data block is not exactly match the number of leaf index, then some leaf indexNode will contain more data.
     public long residue;
+    //the root of this index object.
     public IndexNode root;
-    public ArrayDeque<String> allData=new ArrayDeque<String>();
+    //store all data the broadcast have. In this program we automatecally generate data.
+    public ArrayDeque<String> allData;
+    //the level of which level need to be replicated. 0 means only replicate the root. plus 1 will move the level lower.
     public long replicationLevel;
 
+
     public Index(long children,long ele,long C,long replicationLevel){
+        allData=new ArrayDeque<String>();
+        //total time slot for all the data.
         long D=ele*C;
         this.replicationLevel=replicationLevel;
+        //generating the data need to be broadcasting.
         for(long i=1;i<=D;i++){
             this.allData.add("Data_"+((i-1)/C+1)+"_"+i%C);
         }
+        //the number of child of every node
         this.children=children;
+        //below are calculating the residue.
         long k=Math.round(Math.log(ele)/Math.log(children));
         long leafNum=(long)Math.pow(children,k-1);
         if((long)Math.pow(children,k)<ele)
@@ -42,7 +52,7 @@ public class Index {
         //children is the number of children of a index member
         long children=3;
         //replication level: if 1 then 1 level below the root need to be replicated.
-        long replicationLevel=2;
+        long replicationLevel=1;
         //initial the index object.
         Index id=new Index(children,ele,C,replicationLevel);
 
--- src/IndexNode.java
+++ src/IndexNode.java
@@ -2,10 +2,26 @@
  * Created by xc9pd on 4/14/2016.
  */
 public class IndexNode {
-    long layer;
-    long selfTime;
-    long childrenNum;
-    long[] childrenTime;
+    //the layer of the index. 0 is the root. one level lower plus 1;
+    public long layer;
+    //the label of the node. for example if it is a data node, the data is item 4, then then fromlabel be 4; the toLabel is 4 too;
+    public long fromLabel;
+    //the toLabel is the node range to;
+    public long toLabel;
+    //childrenNum represent the number of children a node have.
+    public long childrenNum;
+    //the children of a node, the label increasing from left to right of the array;
+    public long[] children;
+    //check if the node a data node or an index node;
+    public boolean isIndex;
+
+    public IndexNode(long fromLabel,long toLabel,long childrenNum,long layer,boolean isIndex){
+        this.layer=layer;
+        this.fromLabel=fromLabel;
+        this.toLabel=toLabel;
+        this.childrenNum=childrenNum;
+        
+    }
 
 
 
